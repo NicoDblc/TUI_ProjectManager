637ab4fd37af41a947a86494fbc81b51a2562760
NicoDblc <nicolaslaniel@pm.me>
2020-10-03 14:07:28 -0400

    * Bases

--- src/structure.rs
+++ src/structure.rs
@@ -11,20 +11,23 @@ trait Completable {
     fn complete(&self);
 }
 
+trait TaskContainer {
+    fn add_task(task_description: String);
+}
+
 struct Project {
     name: String,
     description: String,
     tasks: Vec<Task>,
 }
 
-impl Project {
-    pub fn add_task(task_description: String) {}
-}
+impl TaskContainer for Project {}
 
 struct Task {
     description: String,
     time_spent: i32,
     estimate: i32,
+    sub_tassk: Vec<Task>,
 }
 
-impl
+impl TaskContainer for Task {}
