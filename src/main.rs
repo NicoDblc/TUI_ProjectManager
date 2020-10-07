d7b893be57cb6b5fb1292c0d04d92f3967da6bb9
NicoDblc <nicolaslaniel@pm.me>
2020-10-06 23:25:17 -0400

    * First loop and basic display of main window
    + Design image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 mod structure;
+use std::path;
 
 fn main() {
-    println!("Hello, world!");
+    // let args: Vec<String> = std::env::args().collect();
+    // for a in args {
+    //     println!("{}", a);
+    // }
+
+    let home_path = dirs::home_dir().unwrap();
+    let mut app = structure::Application::new(home_path);
+    while app.is_running {
+        // TODO: handle inputs
+        app.update();
+    }
 }
--- src/structure.rs
+++ src/structure.rs
@@ -1,10 +1,60 @@
-pub struct Folder {
-    active_projects: Vec<Project>,
-    archive_projects: Vec<Project>,
+use serde::Deserialize;
+use serde::Serialize;
+
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::Terminal;
+
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
+
+pub struct Application {
+    terminal: tui::Terminal<CrosstermBackend<io::Stdout>>,
+    active_folder_path: std::path::PathBuf,
+    current_folder_projects: Vec<Project>,
+    pub is_running: bool,
 }
 
-trait Inform {
-    fn display_information(&self);
+// Application: Must display available projects from the ~/.project_manager or from the local folder
+
+impl Application {
+    pub fn new(path: std::path::PathBuf) -> Application {
+        let stdout = io::stdout();
+        let backend = CrosstermBackend::new(stdout);
+        let b_terminal = Terminal::new(backend).unwrap();
+        let app = Application {
+            terminal: b_terminal,
+            active_folder_path: path,
+            current_folder_projects: vec![],
+            is_running: true,
+        };
+        // Initialize the path etc
+        app
+    }
+    fn display_main_window(&mut self) {
+        self.terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(1)
+                .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
+                .split(f.size());
+            let block = Block::default().title("Projects").borders(Borders::ALL);
+            f.render_widget(block, chunks[0]);
+            let block = Block::default().title("Tasks").borders(Borders::ALL);
+            f.render_widget(block, chunks[1]);
+        });
+    }
+    fn display_project_window(&mut self) {
+        // self.terminal.draw(f: F)
+    }
+    pub fn update(&mut self) {
+        self.display_main_window();
+    }
+}
+
+trait InformationDisplay {
+    fn display_description(&self);
+    fn display_full(&self);
 }
 
 trait Completable {
@@ -12,22 +62,55 @@ trait Completable {
 }
 
 trait TaskContainer {
-    fn add_task(task_description: String);
+    fn add_task(&mut self, task_description: String);
 }
 
-struct Project {
+#[derive(Clone, Default, Serialize, Deserialize)]
+pub struct Project {
     name: String,
     description: String,
     tasks: Vec<Task>,
 }
 
-impl TaskContainer for Project {}
+impl TaskContainer for Project {
+    fn add_task(&mut self, task_desctiption: String) {
+        let task = Task::new(task_desctiption);
+        self.tasks.push(task);
+    }
+}
 
+#[derive(Clone, Default, Serialize, Deserialize)]
 struct Task {
     description: String,
     time_spent: i32,
     estimate: i32,
-    sub_tassk: Vec<Task>,
+    sub_tasks: Vec<Task>,
 }
 
-impl TaskContainer for Task {}
+impl Task {
+    fn new(task_description: String) -> Task {
+        Task {
+            description: task_description,
+            time_spent: 0,
+            estimate: 0,
+            sub_tasks: vec![],
+        }
+    }
+}
+
+impl InformationDisplay for Task {
+    fn display_description(&self) {}
+    fn display_full(&self) {}
+}
+
+impl TaskContainer for Task {
+    fn add_task(&mut self, task_desctiption: String) {
+        let task = Task {
+            description: task_desctiption,
+            time_spent: 0,
+            estimate: 0,
+            sub_tasks: vec![],
+        };
+        self.sub_tasks.push(task);
+    }
+}
