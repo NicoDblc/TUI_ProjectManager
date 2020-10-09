70086e4103effe0ce8f07b548add59ae782e10b3
NicoDblc <nicolaslaniel@pm.me>
2020-10-09 01:51:34 -0400

    * Loading serialized files
    * Improvement on ui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod structure;
-use std::path;
+use crossterm::event::{self, Event as CEvent, KeyCode};
+use std::{
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
 
 fn main() {
     // let args: Vec<String> = std::env::args().collect();
@@ -8,9 +18,41 @@ fn main() {
     // }
 
     let home_path = dirs::home_dir().unwrap();
+    let (tx, rx) = mpsc::channel();
+
+    let tick_rate = Duration::from_millis(250);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            // poll for tick rate duration, if no events, sent tick event.
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+            if event::poll(timeout).unwrap() {
+                if let CEvent::Key(key) = event::read().unwrap() {
+                    tx.send(Event::Input(key)).unwrap();
+                }
+            }
+            if last_tick.elapsed() >= tick_rate {
+                tx.send(Event::Tick).unwrap();
+                last_tick = Instant::now();
+            }
+        }
+    });
+
     let mut app = structure::Application::new(home_path);
     while app.is_running {
-        // TODO: handle inputs
         app.update();
+        match rx.recv().unwrap() {
+            Event::Input(event) => match event.code {
+                // KeyCode::Char(c) => app.on_key(c),
+                // KeyCode::Left => app.on_left(),
+                KeyCode::Up => app.press_up(),
+                // KeyCode::Right => app.on_right(),
+                KeyCode::Down => app.press_down(),
+                _ => {}
+            },
+            Event::Tick => {}
+        }
     }
 }
--- src/structure.rs
+++ src/structure.rs
@@ -5,46 +5,128 @@ use std::io;
 use tui::backend::CrosstermBackend;
 use tui::Terminal;
 
+use std::ffi::OsStr;
+use std::fs::ReadDir;
 use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Modifier, Style};
 use tui::text::Text;
-use tui::widgets::{Block, Borders, List, ListItem, Paragraph, Widget};
+use tui::widgets::{Block, Borders, List, ListItem, ListState, Paragraph, Widget};
 
+static PROJECT_FILE_EXTENSION: &str = "pman";
+// Application: Must display available projects from the ~/.project_manager or from the local folder
+#[derive(Default)]
+struct DisplayList<T> {
+    state: ListState,
+    array: Vec<T>,
+}
+
+impl<T> DisplayList<T> {
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i < self.array.len() {
+                    i + 1
+                } else {
+                    0
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i > 0 {
+                    i - 1
+                }else {
+                    0
+                }
+
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
 pub struct Application {
     terminal: tui::Terminal<CrosstermBackend<io::Stdout>>,
     active_folder_path: std::path::PathBuf,
-    current_folder_projects: Vec<Project>,
+    project_list: DisplayList<Project>,
     pub is_running: bool,
 }
 
-// Application: Must display available projects from the ~/.project_manager or from the local folder
-
 impl Application {
     pub fn new(path: std::path::PathBuf) -> Application {
-        let stdout = io::stdout();
-        let backend = CrosstermBackend::new(stdout);
-        let b_terminal = Terminal::new(backend).unwrap();
-        let mut app = Application {
-            terminal: b_terminal,
-            active_folder_path: path,
-            current_folder_projects: vec![],
-            is_running: true,
-        };
-        app.terminal.clear().unwrap();
-        for i in 1..10 {
-            app.current_folder_projects
-                .push(Project::new(i.to_string()));
+        // load all project files from the path
+        // TODO open the .pman folder instead and read projects from there
+        let folder_result = std::fs::read_dir(path.as_path()).unwrap();
+        let mut serialized_projects: Vec<Project> = vec![];
+        for file in folder_result {
+            let f = file.unwrap();
+            if f.file_type().unwrap().is_file() {
+                let extension = match f.path().extension() {
+                    Some(ext) => {
+                        if ext == PROJECT_FILE_EXTENSION {
+                            match match serde_json::from_str(
+                                std::fs::read_to_string(f.path()).unwrap().as_str(),
+                            ) {
+                                Ok(result) => Some(result),
+                                Err(E) => None,
+                            } {
+                                Some(project) => serialized_projects.push(project),
+                                _ => {}
+                            }
+                        }
+                    }
+                    _ => {}
+                };
+                // if f.path().extension().unwrap() == PROJECT_FILE_EXTENSION {
+                //     match match serde_json::from_str(
+                //         std::fs::read_to_string(f.path()).unwrap().as_str(),
+                //     ) {
+                //         Ok(result) => Some(result),
+                //         Err(E) => None,
+                //     } {
+                //         Some(project) => serialized_projects.push(project),
+                //         _ => {}
+                //     };
+                // }
+            }
         }
 
-        // TODO Initialize the path etc
+        let stdout = io::stdout();
+        let backend = CrosstermBackend::new(stdout);
+        let mut b_terminal = Terminal::new(backend).unwrap();
+        b_terminal.clear().unwrap();
+        let app = Application {
+            terminal: b_terminal,
+            active_folder_path: path,
+            project_list: DisplayList::default(),
+            is_running: true,
+        };
+        // adding test projects
+        for i in 1..10 {
+            // app.current_folder_projects
+            //     .push(Project::new(i.to_string()));
+        }
+
         app
+    }
+    pub fn press_up(&mut self) {
+        self.project_list.previous();
+    }
+    pub fn press_down(&mut self) {
+        self.project_list.next();
     }
     fn display_main_window(&mut self) {
         let text_active_path = Text::from(self.active_folder_path.to_str().unwrap());
         let mut projects_list_viz: Vec<ListItem> = vec![];
-        for p in &self.current_folder_projects {
+        for p in &self.project_list.array {
             projects_list_viz.push(ListItem::new(Text::from(p.name.clone())));
         }
-        // get current index of the selected Project and Create a Text item containg the description
+
+        let project_list = &self.project_list;
         // This shit is getting ugly, I don't like it
         self.terminal
             .draw(|f| {
@@ -55,7 +137,7 @@ impl Application {
 
                 let current_project_path = Paragraph::new(text_active_path);
                 f.render_widget(current_project_path, window_layout[0]);
-                let mut main_layout = Layout::default()
+                let main_layout = Layout::default()
                     .direction(Direction::Horizontal)
                     .margin(1)
                     .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
@@ -69,24 +151,77 @@ impl Application {
                 let block = Block::default().title("Projects").borders(Borders::ALL);
                 //f.render_widget(block, project_layout[0]);
                 // Project lists
-                let p_list = List::new(projects_list_viz).block(block);
+                let p_list = List::new(projects_list_viz)
+                    .block(block)
+                    .highlight_style(
+                        tui::style::Style::default()
+                            .bg(tui::style::Color::DarkGray)
+                            .add_modifier(tui::style::Modifier::BOLD),
+                    )
+                    .highlight_symbol("-> ");
                 f.render_widget(p_list, project_layout[0]);
 
                 let block = Block::default()
                     .title("Project description")
                     .borders(Borders::ALL);
-                f.render_widget(block, project_layout[1]);
+                let p_description = match project_list.array.len() > 0 {
+                    true => Paragraph::new(
+                        project_list.array[project_list.state.selected().unwrap()]
+                            .clone()
+                            .description,
+                    )
+                    .block(block),
+                    false => Paragraph::new("").block(block),
+                };
+
+                f.render_widget(p_description, project_layout[1]);
+
                 let task_layout = Layout::default()
                     .direction(Direction::Vertical)
                     .margin(0)
                     .constraints([Constraint::Percentage(50), Constraint::Percentage(50)].as_ref())
                     .split(main_layout[1]);
                 let block = Block::default().title("Active tasks").borders(Borders::ALL);
-                f.render_widget(block, task_layout[0]);
+                // TODO create List with task items
+
+                let new_list: Vec<ListItem> = match project_list.array.len() > 0 {
+                    true => {
+                        project_list.array
+                            [project_list.state.selected().unwrap()]
+                            .active_tasks
+                            .clone()
+                            .into_iter()
+                            .map(|a| ListItem::new(Text::from(a.description)))
+                            .collect()
+                    },
+                    false => {
+                        Vec::new()
+                    }
+                };
+
+                let current_task_list = List::new(new_list).block(block);
+                f.render_widget(current_task_list, task_layout[0]);
+
                 let block = Block::default()
                     .title("Completed tasks")
                     .borders(Borders::ALL);
-                f.render_widget(block, task_layout[1]);
+                let new_list: Vec<ListItem> = match project_list.array.len() > 0 {
+                    true => {
+                        project_list.array
+                            [project_list.state.selected().unwrap()]
+                            .completed_tasks
+                            .clone()
+                            .into_iter()
+                            .map(|a| ListItem::new(Text::from(a.description)))
+                            .collect()
+                    }
+                    false => {
+                        Vec::new()
+                    }
+                };
+
+                let completed_tasks_list = List::new(new_list).block(block);
+                f.render_widget(completed_tasks_list, task_layout[1]);
             })
             .unwrap();
     }
@@ -115,7 +250,8 @@ trait TaskContainer {
 pub struct Project {
     name: String,
     description: String,
-    tasks: Vec<Task>,
+    active_tasks: Vec<Task>,
+    completed_tasks: Vec<Task>,
 }
 
 impl Project {
@@ -123,7 +259,8 @@ impl Project {
         Project {
             name: project_name,
             description: String::from("Sample description"),
-            tasks: vec![],
+            active_tasks: vec![],
+            completed_tasks: vec![],
         }
     }
 }
@@ -131,7 +268,7 @@ impl Project {
 impl TaskContainer for Project {
     fn add_task(&mut self, task_desctiption: String) {
         let task = Task::new(task_desctiption);
-        self.tasks.push(task);
+        self.active_tasks.push(task);
     }
 }
 
@@ -169,4 +306,14 @@ impl TaskContainer for Task {
         };
         self.sub_tasks.push(task);
     }
+}
+
+struct A {
+    word: String,
+    number: u32,
+}
+
+fn some_function() {
+    let some_A: Vec<A> = Vec::new();
+    let phrases: Vec<String> = Vec::new();
 }
